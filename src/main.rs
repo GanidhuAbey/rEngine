205a1ca6c52ac02f87f166ca5b00e2a7252cddf4
Usummon <breakcreak@gmail.com>
2019-10-14 13:25:52 -0700

    added vk surface to window (hopefully will be able to render now)

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,39 @@
 //rEngine: a 2D game engine for rust created with "ash", a lightweight wrapper around vulkan.
 use winit::EventsLoop;
+use winit::WindowBuilder;
 use winit::Window;
 
-fn window() {
-    let event_loop = EventsLoop::new();
-    let mut window = Window::new(&event_loop).unwrap();
+use std::sync::Arc;
+
+use vulkano::instance::{Instance};
+use vulkano::swapchain::Surface;
+use vulkano_win::VkSurfaceBuild;
+use vulkano_win::required_extensions;
+
+struct Engine {
+    instance: Arc<Instance>,
+}
+impl Engine {
+    fn new() -> Engine {
+        Engine {
+            instance: {
+                let extensions = required_extensions();
+
+                Instance::new(None, &extensions, None).unwrap()
+            }
+        }
+    }
+    fn create_window(self) -> Arc<Surface<Window>> {
+        let event_loop = EventsLoop::new();
+        WindowBuilder::new().build_vk_surface(&event_loop, self.instance.clone()).unwrap()
+    }
 }
 
 fn main() {
-    let window = window();
+    //Initialize Engine
+    let engine = Engine::new();
+
+    //create window
+    let surface = engine.create_window();
+    let window = surface.window();
 }
