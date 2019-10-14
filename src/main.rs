680f2f1c68582108ee7063e08591e76923ff0141
Usummon <breakcreak@gmail.com>
2019-10-14 11:20:01 -0700

    used the winit crate to add a create window function

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+//rEngine: a 2D game engine for rust created with "ash", a lightweight wrapper around vulkan.
+use winit::EventsLoop;
+use winit::Window;
+
+fn window() {
+    let event_loop = EventsLoop::new();
+    let mut window = Window::new(&event_loop).unwrap();
+}
+
 fn main() {
-    println!("Hello, world!");
+    let window = window();
 }
